9775d6b3d5e8ce4dbbdf994d69c1f9eb323763f1
= <riddicksmith37@gmail.com>
2025-03-10 11:30:12 +0300

    Added file to merge two arrays

---
+++ arrays/mergeArrays.js
@@ -0,0 +1,12 @@
+const WILD_ANIMALS = ["Lion", "Tiger", "Zebra", "Buffalo"];
+const DOMESTIC_ANIMALS = [
+  "Dogs",
+  "Cats",
+  "Hens",
+  "Cows",
+  "Horses",
+  "Donkeys",
+  "Sheet",
+];
+
+const mergedArray = WILD_ANIMALS.concat(...DOMESTIC_ANIMALS);
