baa0a446bb500031ba33603ce3c3b15fae713d1f
= <riddicksmith37@gmail.com>
2025-03-10 11:53:33 +0300

    Updated question description for merge arrays

--- arrays/mergeArrays.js
+++ arrays/mergeArrays.js
@@ -1,3 +1,6 @@
+// Write a program that accepts a group of n domestic animals and a group of x wild animals.
+// Your program should print n + x animals after entering each group.
+
 const WILD_ANIMALS = ["Lion", "Tiger", "Zebra", "Buffalo"];
 const DOMESTIC_ANIMALS = [
   "Dogs",
