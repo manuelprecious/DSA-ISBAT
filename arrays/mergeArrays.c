aa4d3fded7b5ab9366e4fad2f29402053ec03565
= <riddicksmith37@gmail.com>
2025-04-09 05:22:42 +0300

    Code refactory - validation checks

--- arrays/mergeArrays.c
+++ arrays/mergeArrays.c
@@ -1,29 +1,52 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+// Function to check if a string is valid
+bool is_valid_string(const char *str) {
+    if (str == NULL || strlen(str) == 0) {
+        return false;
+    }
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isalpha(str[i]) && str[i] != ' ') {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
+    char wild_animals[4][20] = {"Lion", "Tiger", "Guinea Pig", "Elephant"};
+    char domestic_animals[4][20] = {"Cat", "Cow", "Dog", "Goat"};
+    char new_animals[8][20];
+    int index = 0;
 
-    char *wild_animals[] = {"Lion", "Tiger", "Guinea Pig", "Elephant"};
-    char *domestic_animals[] = {"Cat", "Cow", "Dog", "Goat"};
-    char *new_animals[8];
-    int wild_animal_size = 4;
-    
-    
-    for(int i = 0; i < 4; i++)
-    {
-        //printf("%s\n", domestic_animals[i]);
-        new_animals[i] = wild_animals[i];
-        new_animals[i + wild_animal_size] = domestic_animals[i];
+    // Validate and copy wild animals
+    for (int i = 0; i < 4; i++) {
+        if (is_valid_string(wild_animals[i])) {
+            strcpy(new_animals[index++], wild_animals[i]);
+        } else {
+            printf("Invalid wild animal: %s\n", wild_animals[i]);
+        }
     }
-    
-    for(int i = 0; i < 8; i++)
-    {
+
+    // Validate and copy domestic animals
+    for (int i = 0; i < 4; i++) {
+        if (is_valid_string(domestic_animals[i])) {
+            strcpy(new_animals[index++], domestic_animals[i]);
+        } else {
+            printf("Invalid domestic animal: %s\n", domestic_animals[i]);
+        }
+    }
+
+    // Print valid animals
+    printf("\nValid animals list:\n");
+    for (int i = 0; i < index; i++) {
         printf("%s\n", new_animals[i]);
     }
-
-    // Write C code here
-    //printf("Try programiz.pro");
 
     return 0;
 }
